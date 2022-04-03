9b0ab1436b643c3853a742e81860d720e83674c4
Evan Wolfe <evan.c.wolfe@gmail.com>
2022-04-02 22:12:07 -0500

    4 Cycles? Also fucks any other UIL autos unless they delay - going to try to meepmeep a solution to the ass fucking of the other teams

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlueCarousel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlueCarousel.java
@@ -1,37 +1,10 @@
 package org.firstinspires.ftc.teamcode.auto;
-import com.qualcomm.hardware.rev.RevColorSensorV3;
+
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.trajectory.Trajectory;
-import com.qualcomm.hardware.bosch.BNO055IMU;
-import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
-
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.teamcode.CameraPipelines.CubeDetectionPipeline;
-import org.firstinspires.ftc.teamcode.CameraPipelines.DuckDetectionPipeline;
-import org.firstinspires.ftc.teamcode.CameraPipelines.TSEDetectionPipeline;
-import org.firstinspires.ftc.teamcode.PIDS.LiftPID;
-import org.firstinspires.ftc.teamcode.CameraPipelines.NewDetectionPipeline;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
-import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
-import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequenceBuilder;
-import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 
 @Autonomous(name = "BlueCarousel")
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RedWarehouse.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RedWarehouse.java
@@ -1,39 +1,10 @@
 package org.firstinspires.ftc.teamcode.auto;
-import com.qualcomm.hardware.rev.RevColorSensorV3;
+
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
-import com.acmerobotics.roadrunner.trajectory.Trajectory;
-import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
-import com.qualcomm.hardware.bosch.BNO055IMU;
-import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
-import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
-
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.teamcode.CameraPipelines.CubeDetectionPipeline;
-import org.firstinspires.ftc.teamcode.CameraPipelines.DuckDetectionPipeline;
-import org.firstinspires.ftc.teamcode.CameraPipelines.TSEDetectionPipeline;
-import org.firstinspires.ftc.teamcode.PIDS.LiftPID;
-import org.firstinspires.ftc.teamcode.CameraPipelines.NewDetectionPipeline;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
-import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
-import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 
 
 @Autonomous(name = "RedWarehouse")
