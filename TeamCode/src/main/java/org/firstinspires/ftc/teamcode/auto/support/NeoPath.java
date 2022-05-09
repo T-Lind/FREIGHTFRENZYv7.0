1a5a24aae30d6d6a75538f7d3603b2e527af2615
T-Lind <82475207+T-Lind@users.noreply.github.com>
2022-05-09 11:45:03 -0500

    Added javadoc comments.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/support/NeoPath.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/support/NeoPath.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode.auto.support;
 /**
  * Parent class for paths that solely are composed of time to wheel velocity functions. Essentially acts as a piecewise function over time for both the left and right velocity.
- * @author Tiernan
  * @license MIT License
  * Last edited 5/5
  */
@@ -18,30 +17,63 @@ public abstract class NeoPath {
         built = true;
     }
 
+    /**
+    * Set the execute time - time it takes to follow this specific NeoPath.
+    * @param d is the new execute time.
+    */
     public void setExecuteTime(double d){
         executeTime = d;
     }
+    /**
+    * Get the current execute time - time it takes to follow this specific NeoPath.
+    * @return the execute time.
+    */
     public double getExecuteTime(){
         return executeTime;
     }
 
+    /**
+    * Set the completed status - not overriden.
+    * @param b is the new status of the path (always going to be true).
+    */
     public void setCompleted(boolean b){
         completed = b;
     }
+    
+    /**
+    * Get the completed status - not overriden.
+    * @return the completed status.
+    */
     public boolean getCompleted(){
         return completed;
     }
 
+    /**
+    * Set the built boolean.
+    * @param b is the build status.
+    */
     public void setBuilt(boolean b){
         built = b;
     }
+    /**
+    * Get the build status of this NeoPath.
+    * @return the built variable (build status).
+    */
     public boolean getBuilt(){
         return built;
     }
 
+    /**
+    * Get the left velocity - meant to be overriden, defaults to zero speed.
+    * @param t is the current time into this specific NeoPath.
+    */
     public double getLeftVelocity(double t){
         return 0;
     }
+    /**
+    * Get the right velocity - meant to be overriden, defaults to zero speed.
+    * @param t is the current time into this specific NeoPath.
+    */
     public double getRightVelocity(double t){
         return 0;
     }
