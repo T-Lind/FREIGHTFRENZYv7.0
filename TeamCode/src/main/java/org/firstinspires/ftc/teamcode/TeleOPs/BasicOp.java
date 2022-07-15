98d658863387f4211b21dd3b60be66af729e553e
T-Lind <tiernanxklindauer@gmail.com>
2022-07-15 11:50:35 -0500

    MEC TELEOP UPDATES: UPDATED THE CONTAINER ADDED FUNCTIONALITY TO SWITCH BETWEEN FIELD/ROBOT CENTRIC ADDED VOICE REMINDERS OF THE TIME INTO A MATCH

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOPs/BasicOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOPs/BasicOp.java
@@ -1,9 +1,12 @@
 package org.firstinspires.ftc.teamcode.TeleOPs;
 
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.hardware.ServoEx;
+import com.arcrobotics.ftclib.hardware.SimpleServo;
+import com.arcrobotics.ftclib.util.Timing;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.TeleOPs.support.TeleOpContainer;
-import org.firstinspires.ftc.teamcode.auto.support.Toggle;
 
 
 /**
@@ -12,7 +15,14 @@ import org.firstinspires.ftc.teamcode.auto.support.Toggle;
  */
 @TeleOp(name="BasicOp")
 public class BasicOp extends TeleOpContainer {
-    Toggle fieldCentric = new Toggle(true);
+    protected ServoEx servo;
+
+    @Override
+    protected void initSpecificMechanisms() {
+        servo = new SimpleServo(
+                hardwareMap, "s1", 0, 180
+        );
+    }
 
     /**
      * Method from TeleOpContainer that must be used in order for the teleOp to work.
@@ -20,13 +30,25 @@ public class BasicOp extends TeleOpContainer {
      */
     @Override
     protected void updateMechanisms() {
-        fieldCentric.updateFallingEdge(gamepad1.a);
-        if(fieldCentric.getToggleState())
-            setFieldCentric();
-        else
-            setRobotCentric();
+        // Use the left bumper to switch whether or not the drive type is field centric
+        updateServo();
 
-        telemetry.addData("Hi!","");
+        telemetry.addData("Time left in match: ", getRemainingMatchTime());
+        telemetry.addData("Field Centric Drive Type", getFieldCentric());
         telemetry.update();
+    }
+
+    private void updateServo() {
+        if(driverOp.getButton(GamepadKeys.Button.X)) {
+            servo.turnToAngle(0);
+        }
+
+        else if(driverOp.getButton(GamepadKeys.Button.Y)) {
+            servo.turnToAngle(90);
+        }
+
+        else if(driverOp.getButton(GamepadKeys.Button.B)) {
+            servo.turnToAngle(180);
+        }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOPs/support/TeleOpContainer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOPs/support/TeleOpContainer.java
@@ -2,28 +2,70 @@ package org.firstinspires.ftc.teamcode.TeleOPs.support;
 
 import com.arcrobotics.ftclib.drivebase.MecanumDrive;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.arcrobotics.ftclib.hardware.RevIMU;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.util.Timing;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.auto.support.Toggle;
 
 abstract public class TeleOpContainer extends OpMode {
+    /**
+     * Drivetrain object from FTCLib
+     */
     protected MecanumDrive drive;
+
+    /**
+     * Streamlined IMU object from FTCLib
+     */
     protected RevIMU imu;
+
+    /**
+     * Gamepad object from FTCLib
+     */
     protected GamepadEx driverOp;
 
-    // Default is false - set to true in an opMode with methods
+    /**
+     * Stores the state of the field centric variable locally
+     */
     private boolean FIELD_CENTRIC = false;
 
-    protected void setRobotCentric(){
-        FIELD_CENTRIC = false;
-    }
-    protected void setFieldCentric(){
-        FIELD_CENTRIC = true;
+    /**
+     * Toggle object to store whether or not the drivetrain is in field centric mode
+     */
+    private Toggle fieldCentric;
+
+    /**
+     * Timer to keep track of the current time into the match
+     */
+    private Timing.Timer matchTimer;
+
+    private boolean matchTimerStarted;
+
+    /**
+     * Method to get the field centric state variable for displaying in telemetry, etc.
+     * @return If the drivetrain is in field centric mode or not
+     */
+    protected boolean getFieldCentric() {
+        return FIELD_CENTRIC;
     }
 
+    /**
+     * Method to get the remaining time used in the match for displaying in telemetry, etc.
+     * @return The time remaining in the match
+     */
+    protected long getRemainingMatchTime(){
+        return matchTimer.remainingTime();
+    }
+
+    /**
+     * Initialize the motors, zero power behavior, drivetrain object, imu, and whatever other
+     * hardware you want to initialize every time
+     */
     @Override
     public void init() {
+
         // constructor takes in frontLeft, frontRight, backLeft, backRight motors
         // IN THAT ORDER
         Motor frontLeft = new Motor(hardwareMap, "FL", Motor.GoBILDA.RPM_312);
@@ -64,75 +106,136 @@ abstract public class TeleOpContainer extends OpMode {
         //
         // (unapologetically stolen from the road-runner-quickstart)
 
+        // Create the IMU object and initialize
         imu = new RevIMU(hardwareMap);
         imu.init();
 
-        // the extended gamepad object
+        // Create the extended gamepad object
         driverOp = new GamepadEx(gamepad1);
 
+        // Create a new toggle object for field-robot centric switching mid match
+        fieldCentric = new Toggle(true);
+
+        // Create the new timer object to keep track of match time
+        matchTimer = new Timing.Timer(120);
+        matchTimerStarted = false;
+
+        // Initialize the mechanisms of the robot
+        initSpecificMechanisms();
     }
-        @Override
-        public void loop(){
 
-            // Driving the mecanum base takes 3 joystick parameters: leftX, leftY, rightX.
-            // These are related to the left stick x value, left stick y value, and
-            // right stick x value respectively. These values are passed in to represent the
-            // strafing speed, the forward speed, and the turning speed of the robot frame
-            // respectively from [-1, 1].
-
-            if (!FIELD_CENTRIC) {
-
-                // For a robot centric model, the input of (0,1,0) for (leftX, leftY, rightX)
-                // will move the robot in the direction of its current heading. Every movement
-                // is relative to the frame of the robot itself.
-                //
-                //                 (0,1,0)
-                //                   /
-                //                  /
-                //           ______/_____
-                //          /           /
-                //         /           /
-                //        /___________/
-                //           ____________
-                //          /  (0,0,1)  /
-                //         /     ↻     /
-                //        /___________/
-
-                // optional fourth parameter for squared inputs
-                drive.driveRobotCentric(
-                        driverOp.getLeftX(),
-                        driverOp.getLeftY(),
-                        driverOp.getRightX(),
-                        false
-                );
-            } else {
-
-                // Below is a model for how field centric will drive when given the inputs
-                // for (leftX, leftY, rightX). As you can see, for (0,1,0), it will travel forward
-                // regardless of the heading. For (1,0,0) it will strafe right (ref to the 0 heading)
-                // regardless of the heading.
-                //
-                //                   heading
-                //                     /
-                //            (0,1,0) /
-                //               |   /
-                //               |  /
-                //            ___|_/_____
-                //          /           /
-                //         /           / ---------- (1,0,0)
-                //        /__________ /
-
-                // optional fifth parameter for squared inputs
-                drive.driveFieldCentric(
-                        driverOp.getLeftX(),
-                        driverOp.getLeftY(),
-                        driverOp.getRightX(),
-                        imu.getRotation2d().getDegrees(),   // gyro value passed in here must be in degrees
-                        false
-                );
-            }
-            updateMechanisms();
+    /**
+     * Method of teleop code to loop over every time
+     */
+    @Override
+    public void loop(){
+        // Statement to only run once at the beginning of the loop
+        if(!matchTimerStarted) {
+            matchTimer.start();
+            matchTimerStarted = true;
         }
 
-        abstract protected void updateMechanisms();
+
+        // Driving the mecanum base takes 3 joystick parameters: leftX, leftY, rightX.
+        // These are related to the left stick x value, left stick y value, and
+        // right stick x value respectively. These values are passed in to represent the
+        // strafing speed, the forward speed, and the turning speed of the robot frame
+        // respectively from [-1, 1].
+
+        if (!FIELD_CENTRIC) {
+
+            // For a robot centric model, the input of (0,1,0) for (leftX, leftY, rightX)
+            // will move the robot in the direction of its current heading. Every movement
+            // is relative to the frame of the robot itself.
+            //
+            //                 (0,1,0)
+            //                   /
+            //                  /
+            //           ______/_____
+            //          /           /
+            //         /           /
+            //        /___________/
+            //           ____________
+            //          /  (0,0,1)  /
+            //         /     ↻     /
+            //        /___________/
+
+            // optional fourth parameter for squared inputs
+            drive.driveRobotCentric(
+                    driverOp.getLeftX(),
+                    driverOp.getLeftY(),
+                    driverOp.getRightX(),
+                    false
+            );
+        } else {
+
+            // Below is a model for how field centric will drive when given the inputs
+            // for (leftX, leftY, rightX). As you can see, for (0,1,0), it will travel forward
+            // regardless of the heading. For (1,0,0) it will strafe right (ref to the 0 heading)
+            // regardless of the heading.
+            //
+            //                   heading
+            //                     /
+            //            (0,1,0) /
+            //               |   /
+            //               |  /
+            //            ___|_/_____
+            //          /           /
+            //         /           / ---------- (1,0,0)
+            //        /__________ /
+
+            // optional fifth parameter for squared inputs
+            drive.driveFieldCentric(
+                    driverOp.getLeftX(),
+                    driverOp.getLeftY(),
+                    driverOp.getRightX(),
+                    imu.getRotation2d().getDegrees(),   // gyro value passed in here must be in degrees
+                    false
+            );
+        }
+        // Update what type of driving type is used
+        updateDriveType();
+
+        // Alert the driver to the amount of time left through a voice command.
+        alertToTiming();
+
+        // Update the mechanisms
+        updateMechanisms();
+    }
+
+    /**
+     * Check to see whether or not to change the driving type
+     */
+    private void updateDriveType(){
+        // Use the left bumper to switch whether or not the drive type is field centric
+        fieldCentric.updateLeadingEdge(driverOp.getButton(GamepadKeys.Button.LEFT_BUMPER));
+        FIELD_CENTRIC = fieldCentric.getToggleState();
+    }
+
+    /**
+     * Alert the driver at different time intervals. Note that it takes a while to say the words
+     * so they're started speaking ahead of where they should be.
+     */
+    private void alertToTiming() {
+        if(matchTimer.elapsedTime() == 30)
+            telemetry.speak("30 seconds into tele-op");
+        else if(matchTimer.remainingTime() == 45)
+            telemetry.speak("15 seconds until end game");
+        else if(matchTimer.remainingTime() == 5)
+            telemetry.speak("three");
+        else if(matchTimer.remainingTime() == 3)
+            telemetry.speak("two");
+        else if(matchTimer.remainingTime() == 1)
+            telemetry.speak("one");
+    }
+
+    /**
+     * Abstract method to initialize specific mechanisms you want for a teleop
+     */
+    abstract protected void initSpecificMechanisms();
+
+    /**
+     * Abstract method to update any mechanisms that you're running
+     */
+    abstract protected void updateMechanisms();
 }
