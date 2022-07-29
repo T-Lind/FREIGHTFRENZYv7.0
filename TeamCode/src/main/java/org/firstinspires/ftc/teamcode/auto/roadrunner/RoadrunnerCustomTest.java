c3f37db1f733e83b9c9b8b99ce1fe38a07f16f23
T-Lind <tiernanxklindauer@gmail.com>
2022-07-29 11:35:57 -0500

    wrapper for roadrunner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/roadrunner/RoadrunnerCustomTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/roadrunner/RoadrunnerCustomTest.java
@@ -7,8 +7,11 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.auto.roadrunner.wrapper.SequenceWrapper;
+import org.firstinspires.ftc.teamcode.auto.roadrunner.wrapper.WrapperBuilder;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequenceBuilder;
 
 @Autonomous(name="RoadrunnerCustomTest")
 public class RoadrunnerCustomTest extends LinearOpMode {
@@ -29,22 +32,36 @@ public class RoadrunnerCustomTest extends LinearOpMode {
 
         drive.setPoseEstimate(startPose);
 
-        TrajectorySequence trajectorySequence = drive.trajectorySequenceBuilder(startPose)
-                .splineTo(new Vector2d(10, 0), 0)
-                .splineTo(new Vector2d(10,10),Math.toRadians(90))
-                .addTemporalMarker(1, () -> {
-                    telemetry.addData("","Ran!");
-                    telemetry.update();
-                })
-                .splineToLinearHeading(new Pose2d(10, 20), 0)
-                .back(10)
-                .turn(Math.toRadians(90))
-                .build();
+        SequenceWrapper wrapper = new SequenceWrapper(
+                new WrapperBuilder(drive, startPose)
+                        .splineTo(20, 10, 90)
+        );
+
+
+        TrajectorySequence trajectorySequence = wrapper.trajectorySequenceBuilder.build();
 
         waitForStart();
 
         if(isStopRequested()) return;
 
         drive.followTrajectorySequence(trajectorySequence);
+
+//        TrajectorySequence trajectorySequence = drive.trajectorySequenceBuilder(startPose)
+//                .splineTo(new Vector2d(10, 0), 0)
+//                .splineTo(new Vector2d(10,10),Math.toRadians(90))
+//                .addTemporalMarker(1, () -> {
+//                    telemetry.addData("","Ran!");
+//                    telemetry.update();
+//                })
+//                .splineToLinearHeading(new Pose2d(10, 20), 0)
+//                .back(10)
+//                .turn(Math.toRadians(90))
+//                .build();
+//
+//        waitForStart();
+//
+//        if(isStopRequested()) return;
+//
+//        drive.followTrajectorySequence(trajectorySequence);
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/roadrunner/wrapper/WrapperBuilder.java
@@ -0,0 +1,187 @@
+package org.firstinspires.ftc.teamcode.auto.roadrunner.wrapper;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.MarkerCallback;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequenceBuilder;
+
+public class WrapperBuilder {
+    public TrajectorySequenceBuilder trajectorySequenceBuilder;
+    private boolean metric;
+
+    public WrapperBuilder(SampleMecanumDrive drive, Pose2d startPose) {
+        trajectorySequenceBuilder = drive.trajectorySequenceBuilder(startPose);
+        metric = false;
+    }
+
+    /**
+     * Use *CENTIMETERS* INSTEAD OF INCHES
+     * @return this object to use in a builder design pattern
+     */
+    public WrapperBuilder metric() {
+        metric = true;
+        return this;
+    }
+
+    public WrapperBuilder forward(double distance) {
+        if(metric) {
+            trajectorySequenceBuilder.forward(toInchesFromCentimeters(distance));
+            return this;
+        }
+        trajectorySequenceBuilder.forward(distance);
+        return this;
+    }
+
+    public WrapperBuilder back(double distance) {
+        if(metric) {
+            trajectorySequenceBuilder.back(toInchesFromCentimeters(distance));
+            return this;
+        }
+
+        trajectorySequenceBuilder.back(distance);
+        return this;
+    }
+
+    public WrapperBuilder strafeLeft(double distance) {
+        if(metric) {
+            trajectorySequenceBuilder.strafeLeft(toInchesFromCentimeters(distance));
+            return this;
+        }
+
+        trajectorySequenceBuilder.strafeLeft(distance);
+        return this;
+    }
+
+    public WrapperBuilder strafeRight(double distance) {
+        if(metric) {
+            trajectorySequenceBuilder.strafeRight(toInchesFromCentimeters(distance));
+            return this;
+        }
+
+        trajectorySequenceBuilder.strafeRight(distance);
+        return this;
+    }
+
+    public WrapperBuilder strafeTo(double x, double y) {
+        if(metric) {
+            trajectorySequenceBuilder.strafeTo(new Vector2d(toInchesFromCentimeters(x), toInchesFromCentimeters(y)));
+            return this;
+        }
+
+        trajectorySequenceBuilder.strafeTo(new Vector2d(x, y));
+        return this;
+    }
+
+    public WrapperBuilder lineTo(double x, double y) {
+        if(metric) {
+            trajectorySequenceBuilder.lineTo(new Vector2d(toInchesFromCentimeters(x), toInchesFromCentimeters(y)));
+            return this;
+        }
+
+        trajectorySequenceBuilder.lineTo(new Vector2d(x, y));
+        return this;
+    }
+
+    public WrapperBuilder lineToConstantHeading(double x, double y) {
+        if(metric) {
+            trajectorySequenceBuilder.lineToConstantHeading(new Vector2d(toInchesFromCentimeters(x), toInchesFromCentimeters(y)));
+            return this;
+        }
+
+        trajectorySequenceBuilder.lineToConstantHeading(new Vector2d(x, y));
+        return this;
+    }
+
+    public WrapperBuilder lineToLinearHeading(double x, double y, double endHeading) {
+        if(metric) {
+            trajectorySequenceBuilder.lineToLinearHeading(new Pose2d(toInchesFromCentimeters(x), toInchesFromCentimeters(y), Math.toRadians(endHeading)));
+            return this;
+        }
+
+        trajectorySequenceBuilder.lineToLinearHeading(new Pose2d(x, y, Math.toRadians(endHeading)));
+        return this;
+    }
+
+    public WrapperBuilder lineToSplineHeading(double x, double y, double endHeading) {
+        if(metric) {
+            trajectorySequenceBuilder.lineToSplineHeading(new Pose2d(toInchesFromCentimeters(x),
+                    toInchesFromCentimeters(y), Math.toRadians(endHeading)));
+            return this;
+        }
+
+        trajectorySequenceBuilder.lineToSplineHeading(new Pose2d(x, y, Math.toRadians(endHeading)));
+        return this;
+    }
+
+
+    public WrapperBuilder splineTo(double x, double y, double endHeading) {
+        if(metric) {
+            trajectorySequenceBuilder.splineTo(new Vector2d(toInchesFromCentimeters(x),
+                    toInchesFromCentimeters(y)), Math.toRadians(endHeading));
+            return this;
+        }
+
+        trajectorySequenceBuilder.splineTo(new Vector2d(x, y), Math.toRadians(endHeading));
+        return this;
+    }
+
+    public WrapperBuilder splineToConstantHeading(double x, double y, double endTangent) {
+        if(metric) {
+            trajectorySequenceBuilder.splineToConstantHeading(new Vector2d(toInchesFromCentimeters(x),
+                    toInchesFromCentimeters(y)), Math.toRadians(endTangent));
+            return this;
+        }
+
+        trajectorySequenceBuilder.splineToConstantHeading(new Vector2d(x, y), Math.toRadians(endTangent));
+        return this;
+    }
+
+    public WrapperBuilder splineToLinearHeading(double x, double y, double endHeading, double endTangent) {
+        if(metric) {
+            trajectorySequenceBuilder.splineToLinearHeading(new Pose2d(toInchesFromCentimeters(x),
+                    toInchesFromCentimeters(y), Math.toRadians(endHeading)), Math.toRadians(endTangent));
+            return this;
+        }
+
+        trajectorySequenceBuilder.splineToLinearHeading(new Pose2d(x, y, Math.toRadians(endHeading)),
+                Math.toRadians(endTangent));
+        return this;
+    }
+
+    public WrapperBuilder splineToSplineHeading(double x, double y, double endHeading, double endTangent) {
+        if(metric) {
+            trajectorySequenceBuilder.splineToSplineHeading(new Pose2d(toInchesFromCentimeters(x),
+                    toInchesFromCentimeters(y), Math.toRadians(endHeading)), Math.toRadians(endTangent));
+            return this;
+        }
+
+        trajectorySequenceBuilder.splineToSplineHeading(new Pose2d(x, y, Math.toRadians(endHeading)),
+                Math.toRadians(endTangent));
+        return this;
+    }
+
+    public WrapperBuilder turn(double angle) {
+        trajectorySequenceBuilder.turn(Math.toRadians(angle));
+        return this;
+    }
+
+    public WrapperBuilder addTemporalMarker(double time, MarkerCallback callback) {
+        trajectorySequenceBuilder.addTemporalMarker(0.0, time, callback);
+        return this;
+    }
+
+
+    public WrapperBuilder build() {
+        trajectorySequenceBuilder.build();
+        return this;
+    }
+
+    private double toInchesFromCentimeters(double centimeters) {
+        return 2.54*centimeters;
+    }
+}
